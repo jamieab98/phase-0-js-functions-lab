76aa9824f9700089aabb0c60e492aca73f8a437f
Jamie Bryson <jamieab98@gmail.com>
2025-07-16 11:14:50 -0400

    finished the assignment

--- index.js
+++ index.js
@@ -20,8 +20,34 @@ function findMaximum(num1, num2) {
 console.log("The larger of these two numbers: ", findMaximum(3, 2))
 
 function isPalindrome(word) {
-
+    let originalArray = word.toLowerCase().split("");
+    let arrayLength = originalArray.length - 1;
+    for (let i = 0; i < arrayLength; i++) {
+        let palindrome = true;
+        if (originalArray[i] !== originalArray[arrayLength - i]){
+            i = arrayLength;
+            palindrome = false;
+            return palindrome;
+            //console.log(i)
+            //console.log("not a palindrome");
+            //console.log(palindrome);
+        } else if (i > (arrayLength / 2)) {
+            i = arrayLength;
+            palindrome = true;
+            return palindrome;
+            //console.log("Halfway is good enough");
+            //console.log(i);
+            //console.log("This is a palindrome");
+            //console.log(palindrome);
+        } else {
+            //console.log("Still going");
+            //console.log(i);
+            //console.log("This might be a palindrome");
+        }
+    }
 }
+console.log(isPalindrome("racecar"))
+
 
 function calculateDiscountedPrice(originalPrice, discountedPercentage) {
     let discountedPrice = originalPrice - (originalPrice * (discountedPercentage / 100));
