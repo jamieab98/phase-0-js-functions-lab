86d23d7ee2476abe2fc50f8f5c3af74aa71acfb7
Jamie Bryson <jamieab98@gmail.com>
2025-07-16 15:11:59 -0400

    Made more specific based off the rubric

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function calculateTax(amount) {
-    let tax = amount * 0.1;
-    return tax;
+    if (amount >= 0) {
+        let tax = amount * 0.1;
+        console.log("The tax is: ", tax);
+    } else {
+        console.log("Error. Please enter a positive number");
+    }
 }
-console.log("The tax on $2 is: ", calculateTax(2))
+calculateTax(4)
 
 function convertToUpperCase(text) {
     let txt = text.toUpperCase();
     return txt;
 }
-console.log(convertToUpperCase("Hello"))
+console.log(convertToUpperCase("H3y"))
 
 function findMaximum(num1, num2) {
     if (num1 > num2) {
@@ -17,45 +21,45 @@ function findMaximum(num1, num2) {
         return num2;
     }
 }
-console.log("The larger of these two numbers: ", findMaximum(3, 2))
+console.log("The larger of these two numbers: ", findMaximum(-3, -3))
 
 function isPalindrome(word) {
     let originalArray = word.toLowerCase().split("");
     let arrayLength = originalArray.length - 1;
-    for (let i = 0; i < arrayLength; i++) {
-        let palindrome = true;
-        if (originalArray[i] !== originalArray[arrayLength - i]){
-            i = arrayLength;
-            palindrome = false;
-            return palindrome;
-            //console.log(i)
-            //console.log("not a palindrome");
-            //console.log(palindrome);
-        } else if (i > (arrayLength / 2)) {
-            i = arrayLength;
-            palindrome = true;
-            return palindrome;
-            //console.log("Halfway is good enough");
-            //console.log(i);
-            //console.log("This is a palindrome");
-            //console.log(palindrome);
-        } else {
-            //console.log("Still going");
-            //console.log(i);
-            //console.log("This might be a palindrome");
+    if (arrayLength > 0) {
+        for (let i = 0; i < arrayLength; i++) {
+            let palindrome = true;
+            if (originalArray[i] !== originalArray[arrayLength - i]){
+                i = arrayLength;
+                palindrome = false;
+                return palindrome;
+            } else if (i > (arrayLength / 2)) {
+                i = arrayLength;
+                palindrome = true;
+                return palindrome;
+            } else {
+                return palindrome;
+            }
         }
+    } else {
+        let palindrome = true;
+        return palindrome;
     }
 }
-console.log(isPalindrome("racecar"))
-
+console.log(isPalindrome(""))
 
 function calculateDiscountedPrice(originalPrice, discountedPercentage) {
-    let discountedPrice = originalPrice - (originalPrice * (discountedPercentage / 100));
-    return discountedPrice;
+    if (discountedPercentage > 100){
+        return "Error. Discounted percentage cannot be more than 100. Try again";
+    } else {
+        let discountedPrice = originalPrice - (originalPrice * (discountedPercentage / 100));
+        return discountedPrice;
+    }
 }
-console.log(calculateDiscountedPrice(100, 20))
+console.log(calculateDiscountedPrice(50, 100))
+
 
 
 
 // This is required for the test to function properly  
-//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
